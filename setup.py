c9ee834fdb5f1fb846733755d6b4de6b74c7955e
laborautonomo <fabio@laborautonomo.org>
2014-10-08 00:55:48 -0300

    [chg] requirements of setup

--- setup.py
+++ setup.py
@@ -19,7 +19,12 @@ setup(
         'Django app for a simple content management system'
     ),
     long_description=README,
-    install_requires=REQUIREMENTS,
+    install_requires=[
+        'Django>=1.4.10',
+        'django-admin-sortable==1.7.3',
+        'django-ckeditor-updated==4.4.0',
+        '-e git://github.com/laborautonomo/django-la-tags.git#egg=django-la-tags',
+    ],
     author='Fábio Piovam Elias',
     author_email='fabio@laborautonomo.org',
     url='https://github.com/laborautonomo/django-la-cms/',
