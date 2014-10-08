c9ebad73be27a030ff77be7c55959931201fff59
laborautonomo <fabio@laborautonomo.org>
2014-10-08 01:26:16 -0300

    [fix] added dependency_links into setup to call django-la-tags

--- setup.py
+++ setup.py
@@ -19,11 +19,9 @@ setup(
         'Django app for a simple content management system'
     ),
     long_description=README,
-    install_requires=[
-        'Django>=1.4.10',
-        'django-admin-sortable==1.7.3',
-        'django-ckeditor-updated==4.4.0',
-        '-e git://github.com/laborautonomo/django-la-tags.git#egg=django-la-tags',
+    install_requires=REQUIREMENTS,
+    dependency_links = [
+        '-e git://github.com/laborautonomo/django-la-tags.git@v0.1.0#egg=django-la-tags',
     ],
     author='Fábio Piovam Elias',
     author_email='fabio@laborautonomo.org',
