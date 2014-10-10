10f9fe978911415752aeac78ca8c13dafcfd2c2b
laborautonomo <fabio@laborautonomo.org>
2014-10-10 10:21:43 -0300

    [fix] removed "extends..." of CMS templates; [chg] added encoding='utf-8' as parameter of "open" function into setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@
 from setuptools import setup, find_packages
 
 try:
-    README = open('README.md').read()
+    README = open('README.md', encoding='utf-8').read()
 except:
     README = None
 
 try:
-    REQUIREMENTS = open('requirements.txt').read()
+    REQUIREMENTS = open('requirements.txt', encoding='utf-8').read()
 except:
     REQUIREMENTS = None
 
