eb858def12dde33e1a1d559673aebb86d37c432b
laborautonomo <fabio@laborautonomo.org>
2016-02-28 18:38:15 -0300

    [chg] removed 'dependency_links'; [add] 'zip_safe' parameter

--- setup.py
+++ setup.py
@@ -21,12 +21,10 @@ setup(
     ),
     long_description=README,
     install_requires=REQUIREMENTS,
-    dependency_links = [
-        'https://github.com/laborautonomo/django-la-tags/archive/v0.1.0.tar.gz#egg=django-la-tags',
-    ],
     author='Fábio Piovam Elias',
     author_email='fabio@laborautonomo.org',
     url='https://github.com/laborautonomo/django-la-cms/',
     packages=find_packages(),
     include_package_data=True,
+    zip_safe=False,
 )
