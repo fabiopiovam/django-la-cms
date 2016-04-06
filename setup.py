f7dfac274df087abe5ef9b4af50a48cb0df1c126
laborautonomo <fabio@laborautonomo.org>
2016-04-05 23:14:33 -0300

    [fix] install_requires & dependency_links

--- setup.py
+++ setup.py
@@ -1,17 +1,32 @@
 #!/usr/bin/env python
-# -*- coding: UTF-8 -*-
+# -*- coding: utf-8 -*-
 
+import os
+import sys
+
+from pip.req import parse_requirements
 from setuptools import setup, find_packages
+
+# allow setup.py to be run from any path and open files
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 try:
     README = open('README.md').read()
 except:
     README = None
 
-try:
-    REQUIREMENTS = open('requirements.txt').read()
-except:
-    REQUIREMENTS = None
+def get_requirements(filename):
+    install_requires = []
+    dependency_links = []
+    for r in parse_requirements(filename, session=False):
+        if hasattr(r, 'link') and r.link and r.link.url:
+            r.req.specs = [('<=', '99.99')]
+            dependency_links.append(r.link.url+'-99.99')
+            dependency_links.append(r.link.url)
+        install_requires.append(str(r.req))
+    return install_requires, dependency_links
+
+install_requires, dependency_links = get_requirements('requirements.txt')
 
 setup(
     name='django-la-cms',
@@ -20,7 +35,8 @@ setup(
         'Django app for a simple content management system'
     ),
     long_description=README,
-    install_requires=REQUIREMENTS,
+    install_requires=install_requires,
+    dependency_links=dependency_links,
     author='Fábio Piovam Elias',
     author_email='fabio@laborautonomo.org',
     url='https://github.com/laborautonomo/django-la-cms/',
